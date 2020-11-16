d9c60a5eaf4b5d5b1050f0290222477cf09a8655
Yoshihisa Tanaka <yoshihisa.tnk04@gmail.com>
2020-11-16 16:06:18 +0900

    add: pca for other datasets

--- script/data_processing.py
+++ script/data_processing.py
@@ -1,6 +1,6 @@
 # Author: yoshi
 # Date: 9/18/2020
-# Updated:
+# Updated: 11/12/2020
 # Project: CTOS folfoliox project
 # Script: To perform basic analyses
 
@@ -12,6 +12,7 @@ from sklearn.decomposition import PCA
 from pandas import plotting
 
 def dataload(filename):
+    ## load data ##
     data = pd.read_table(filename, sep='\t', header=0, index_col=0)
     print(f'[LOAD]: {filename}')
     print(f'input matrix: {data.shape}')
@@ -19,7 +20,7 @@ def dataload(filename):
 
 
 def scatterplot(matrix, filename):
-    ## Depict scatter plot across all pairwise samples
+    ## Depict scatter plot across all pairwise samples ##
     plt.figure(figsize=(25, 25))
     plotting.scatter_matrix(matrix, diagonal='hist', alpha=0.2, s=1, c='lime')
     plt.savefig(filename, dpi=300, format='png')
@@ -28,6 +29,7 @@ def scatterplot(matrix, filename):
 
 
 def histogram(matrix, filename):
+    ## plot histogram per genes ##
     plt.hist(matrix.mean(axis='columns'), alpha=0.4, bins=30, color='lime')
     plt.xlabel('expression')
     plt.ylabel('frequency')
@@ -53,9 +55,9 @@ def pca(matrix, filename):
     pca_output = pca.transform(data_std.T)
     embed = pd.DataFrame(pca_output, columns=['PC1', 'PC2'], index=samplenames)
 
-    plt.scatter(embed['PC1'], embed['PC2'], alpha=0.6, c='darkmagenta', s=10)
+    plt.scatter(embed['PC1'], embed['PC2'], alpha=0.6, c='mediumblue', s=23)
     for x, y, name in zip(embed['PC1'], embed['PC2'], samplenames):
-        plt.text(x, y, name, alpha=0.6, fontsize=6, weight='bold')
+        plt.text(x, y, name, alpha=0.6, fontsize=7, weight='bold')
     plt.xlabel('PC1')
     plt.ylabel('PC2')
     plt.savefig(filename, dpi=300, format='png')
@@ -64,6 +66,7 @@ def pca(matrix, filename):
 
 
 def violinplot(matrix, filename):
+    ## violinplot per sample to see data distribution ##
     plt.figure(figsize=(15, 8))
     sns.violinplot(data=matrix, inner='box')
     plt.ylabel('expression')
@@ -77,9 +80,38 @@ if __name__ == '__main__':
 
     SCNECdata = dataload('../data/SCNEC_dataset.txt') #theme color: deeppink
     CRCdata = dataload('../data/CRC_dataset.txt') #theme color: lime
+    MIXdata = pd.concat([SCNECdata, CRCdata], join='inner', axis=1)
+    print(f'MIXdata shape: {MIXdata.shape}')
 
-    #MIXdata = pd.concat([SCNECdata, CRCdata], join='inner', axis=1)
-    #print(f'MIXdata shape: {MIXdata.shape}')
+    ## CRC: select float condition (theme color: mediumblue)
+    CRCdata_float = CRCdata.loc[:,['C45-float',
+                                   'C48-float',
+                                   'C75-float',
+                                   'C97-float',
+                                   'C86-float',
+                                   'C111-foat',
+                                   'C132-float',
+                                   'C138-float',
+                                   'C166-float',
+                                   'CB3-float',
+                                   'HT29',
+                                   'SW620']]
+    print(f'CRC-float shape: {CRCdata_float.shape}')
+
+    ## CRC: select non condition
+    CRCdata_non = CRCdata.loc[:,['C45',
+                                 'C48',
+                                 'C75',
+                                 'C97',
+                                 'C86',
+                                 'C111',
+                                 'C132',
+                                 'C138',
+                                 'C166',
+                                 'CB3',
+                                 'HT29',
+                                 'SW620']]
+    print(f'CRC-non shape: {CRCdata_non.shape}')
 
     scatterplot(SCNECdata, '../data/SCNEC_scatterplot.png')
     scatterplot(CRCdata, '../data/CRC_scatterplot.png')
@@ -89,7 +121,9 @@ if __name__ == '__main__':
 
     pca(SCNECdata, '../data/SCNEC_pca.png')
     pca(CRCdata, '../data/CRC_pca.png')
-    #pca(MIXdata, '../data/MIXdata_pca.png') 
+    pca(MIXdata, '../data/MIXdata_pca.png') 
+    pca(CRCdata_float, '../data/CRC_float_pca.png')
+    pca(CRCdata_non, '../data/CRC_non_pca.png')
 
     violinplot(SCNECdata, '../data/SCNEC_violinplot.png')
     violinplot(CRCdata, '../data/CRC_violinplot.png')
